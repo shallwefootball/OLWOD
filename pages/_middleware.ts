bb7b17435f5fa06c11dc687e7cce6860da9d8726
amos <amos@shallwefootball.com>
2023-01-16 22:12:14 +0900

    응답데이터 수정

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,22 +5,27 @@ const HASH_TAG = /#[\d|A-Z|a-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*/g;
 export async function middleware(req: NextRequest) {
   const instaHeaders = new Headers({ "X-Ig-App-Id": process.env.IG_APP_ID });
 
-  let text;
+  let resEj;
   try {
-    const resEj = await fetch(
+    const res = await fetch(
       `https://www.instagram.com/api/v1/users/web_profile_info/?username=${process.env.TARGET_ACCOUNT}`,
       {
         headers: instaHeaders,
       }
-    ).then((res) => res.json());
-
-    const { edges } = resEj.data.user.edge_owner_to_timeline_media;
-    const { edge_media_to_caption } = edges[0].node;
-    text = edge_media_to_caption.edges[0].node.text;
-    text = text.replace(HASH_TAG, "");
-  } catch (e) {
-    text = e.toString();
+    );
+    resEj = await res.json();
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      // Unexpected token < in JSON
+      console.log("There was a SyntaxError", error);
+    } else {
+      console.log("There was an error", error);
+    }
   }
+
+  const { edges } = resEj.data.user.edge_owner_to_timeline_media;
+  const { edge_media_to_caption } = edges[0].node;
+  const text = edge_media_to_caption.edges[0].node.text.replace(HASH_TAG, "");
   const body = JSON.stringify({ text });
 
   const channelHeaders = new Headers({ contentType: "application/json" });
